395522eee1d4345fa943226fd458e33738e52d3d
Junyuan Hong <jyhong836@gmail.com>
2015-09-30 17:01:54 +0800

    Add playground.

---
+++ Markingbird/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Markingbird
+
+var markdownText = "Hello, playground, `func a()` \n\n + a1 \n + a2"
+
+var markdownEngine = Markdown()
+let informationHTML = markdownEngine.transform(markdownText)
+
+let informationData = informationHTML.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+let informationString = try NSAttributedString(data: informationData!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil)
