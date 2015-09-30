ca1be66040915baf9c4c98849a74d2a9abe2f8a8
Junyuan Hong <jyhong836@gmail.com>
2015-09-30 20:56:24 +0800

    .

--- Markingbird/MyPlayground.playground/Contents.swift
+++ Markingbird/MyPlayground.playground/Contents.swift
@@ -3,10 +3,14 @@
 import Cocoa
 import Markingbird
 
-var markdownText = "Hello, playground, `func a()` \n\n + a1 \n + a2"
+var markdownText = "Hello, playground, `func a()` \n\n + ~~a1~~ \n + a2"
 
 var markdownEngine = Markdown()
 let informationHTML = markdownEngine.transform(markdownText)
 
 let informationData = informationHTML.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
 let informationString = try NSAttributedString(data: informationData!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil)
+informationString.length
+markdownText.characters.count
+informationString.string.characters.count
+print(informationString.string)
